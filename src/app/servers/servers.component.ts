2acfd8c2163da0df6b9c6600402418755cf0548b
shubhsurya06 <shubhsurya06@gmail.com>
2020-08-15 01:50:47 +0530

    initial commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  // template: `<h1>Hello, Shubham Suryawanshi, hope you are doing really well?</h1> `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
